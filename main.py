cc48cc543f67e5ca667b90056d046de55460f916
Abshir Mohamed <you@example.comAbshir_4@hotmail.com>
2021-09-29 13:25:00 +0200

    merge conflict solved

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     print("this is a cool app")
     for i in range(100):
+        print("weeheh")
         print("This is a message")
+        
 
 if __name__ == '__main__':
     main()
